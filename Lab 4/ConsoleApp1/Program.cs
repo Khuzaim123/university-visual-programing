9997e0a4b9411dce9abeddf3b949c7fd103ea81f
Khuzaim123 <133508881+Khuzaim123@users.noreply.github.com>
2024-10-16 19:25:09 +0500

    Add files via upload

---
+++ Lab 4/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Question1
+{
+    static void Main()
+    {
+        Student st = new Student();
+        st.setname("Khuzaim");
+        st.setid(233587);
+        st.setage(19);
+        st.setdep("BSCS");
+        st.display();
+    }
+}
