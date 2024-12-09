d80c9a25232a7f2899eef793649e33fe087eb8a2
Khuzaim123 <133508881+Khuzaim123@users.noreply.github.com>
2024-12-09 21:24:27 +0500

    Add files via upload

---
+++ Assigment 4/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string>li = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            if(text.Text != "")
+            {
+                if (!li.Contains(text.Text))
+                {
+                    li.Add(text.Text);
+                    players.ItemsSource = null;
+                    players.ItemsSource = li;
+                    text.Text = "";
+                    MessageBox.Show("Player has been added sucessfully!");
+                }
+                else
+                {
+                    MessageBox.Show("This player is already in the list");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please! Enter name of the player");
+            }
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+            li.RemoveAt(players.SelectedIndex);
+            players.ItemsSource = null;
+            players.ItemsSource = li;
+            MessageBox.Show("Player has been deleted sucessfully!");
+        }
+    }
+}
