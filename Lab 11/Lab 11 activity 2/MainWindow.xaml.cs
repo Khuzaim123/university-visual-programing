b33c9d4c5efc6a1736da0a318a3cb16250472d76
Khuzaim123 <133508881+Khuzaim123@users.noreply.github.com>
2024-12-11 14:55:03 +0500

    Add files via upload

---
+++ Lab 11/Lab 11 activity 2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Activity3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void check1_Checked(object sender, RoutedEventArgs e)
+        {
+            text1.Text = "Check Box 1 Checked";
+        }
+
+        private void check2_Checked(object sender, RoutedEventArgs e)
+        {
+            text2.Text = "Check Box 2 Checked";
+        }
+
+        private void check1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            text1.Text = "";
+        }
+
+        private void check2_Unchecked(object sender, RoutedEventArgs e)
+        {
+            text2.Text = "";
+        }
+    }
+}
