6a5e750254c4a63ef9041f7d5bb692c28640cf2c
Khuzaim123 <133508881+Khuzaim123@users.noreply.github.com>
2025-01-10 21:44:34 +0500

    Add files via upload

---
+++ Assignment5/Cities.cs
@@ -0,0 +1,12 @@
+﻿public class Cities
+{
+    public string name;
+    public int id;
+    public string servername;
+    public Cities()
+    {
+        name = string.Empty;
+        id = 0;
+        servername = string.Empty;
+    }
+}
---
+++ Assignment5/CityList.cs
@@ -0,0 +1,12 @@
+﻿public static class CityList
+{
+    public static List<Cities> list = new List<Cities>();
+    public static void addcity(Cities c)
+    {
+        list.Add(c);
+    }
+    public static List<Cities> getcity(Cities c)
+    {
+        return list;
+    }
+}
